8d74657ed1f7fa88e1360c15a90cb97fd85651d4
Mohammad Mahmoodi <mm580486@gmail.com>
2024-12-05 14:38:38 +0330

    Init commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in parsbank.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/parsbank.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "parsbank/version"
+
+module Parsbank
+  class Error < StandardError; end
+  # Your code goes here...
+end
